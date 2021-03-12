56014c48dae08509e77b5b7cabe3c2b0142b3c3c
mosguinz <mos.guinz@gmail.com>
2021-03-11 20:28:29 -0800

    Replace iterative approach with recursive approach

--- src/assignment03PartD/PascalsTriangleGenerator.java
+++ src/assignment03PartD/PascalsTriangleGenerator.java
@@ -17,18 +17,28 @@ public class PascalsTriangleGenerator {
     }
 
     public int[] computeRow(int rowToCompute) {
-        int[][] triangle = new int[rowToCompute][];
-        triangle[0] = new int[]{1};
-
-        for (int i = 1; i < triangle.length ; i++) {
-            final int[] previousRow = triangle[i - 1];
-            int[] thisRow = new int[i + 1];
-            thisRow[0] = thisRow[i] = 1;
-            for (int j = 1; j < previousRow.length; j++) {
-                thisRow[j] = previousRow[j - 1] + previousRow[j];
-            }
-            triangle[i] = thisRow;
+        // Base case: first row contains exactly one element: 1.
+        if (rowToCompute == 1) {
+            return new int[]{1};
         }
-        return triangle[rowToCompute - 1];
+
+        // Recursive call to compute the previous row to use in computing `thisRow`.
+        int[] previousRow = computeRow(rowToCompute - 1);
+
+        // Row `n` contains `n` numbers.
+        int[] thisRow = new int[rowToCompute];
+
+        // Row always begin with 1.
+        thisRow[0] = 1;
+
+        // Calculate the interior numbers (non-1s) by summing the ones above.
+        for (int i = 1; i < previousRow.length; i++) {
+            thisRow[i] = previousRow[i - 1] + previousRow[i];
+        }
+
+        // Row always end with 1.
+        thisRow[previousRow.length] = 1;
+
+        return thisRow;
     }
 }
