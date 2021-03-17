989830fd0165d08c81523e58a4826ac63990278e
mosguinz <mos.guinz@gmail.com>
2021-03-17 00:58:13 -0700

    Add Node implementation for Stack

--- src/assignment03PartB/OurStack.java
+++ src/assignment03PartB/OurStack.java
@@ -36,4 +36,36 @@ public class OurStack<T> implements StackInterface<T> {
     @Override
     public void clear() {
     }
+
+    private class Node {
+
+        private Node next;
+        private T data;
+
+        private Node(T data) {
+            this(data, null);
+        }
+
+        private Node(T data, Node nextNode) {
+            this.data = data;
+            this.next = nextNode;
+        }
+
+        public T getData() {
+            return data;
+        }
+
+        public void setData(T data) {
+            this.data = data;
+        }
+
+        public Node getNext() {
+            return next;
+        }
+
+        public void setNext() {
+            this.next = next;
+        }
+
+    }
 }
