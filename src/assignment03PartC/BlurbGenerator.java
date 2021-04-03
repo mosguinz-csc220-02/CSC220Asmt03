42a4338ec53d62994f0bebbca9caf058d9ae0b2f
mosguinz <mos.guinz@gmail.com>
2021-04-02 18:18:27 -0700

    Add documentation

--- src/assignment03PartC/BlurbGenerator.java
+++ src/assignment03PartC/BlurbGenerator.java
@@ -13,7 +13,14 @@ package assignment03PartC;
 
 public class BlurbGenerator {
 
+    /**
+     * Maximum number of 'y's allowed following any given Whoozits.
+     */
     private static final int MAX_WHOOZIT_Y_SIZE = 5;
+
+    /**
+     * Maximum number of Whatzits allowed in any given Blurb.
+     */
     private static final int MAX_WHAZITS = 5;
 
     /**
@@ -36,6 +43,8 @@ public class BlurbGenerator {
 
     /**
      * Generates and returns a random Blurb. A Blurb is a Whoozit followed by one or more Whatzits.
+     *
+     * @return A Blurb.
      */
     public String makeBlurb() {
         return makeWhoozit() + makeMultiWhatzits(getRandInt(1, MAX_WHAZITS));
@@ -43,6 +52,8 @@ public class BlurbGenerator {
 
     /**
      * Generates a random Whoozit. A Whoozit is the character 'x' followed by zero or more 'y's.
+     *
+     * @return A Whoozit.
      */
     private String makeWhoozit() {
         return "x" + makeYString(getRandInt(0, MAX_WHOOZIT_Y_SIZE));
@@ -50,13 +61,19 @@ public class BlurbGenerator {
 
     /**
      * Recursively generates a string of zero or more 'y's.
+     *
+     * @param n Number of 'y's to generate.
+     * @return n-amount of 'y's.
      */
-    private String makeYString(int ySize) {
-        return ySize == 0 ? "" : makeYString(ySize - 1) + "y";
+    private String makeYString(int n) {
+        return n == 0 ? "" : makeYString(n - 1) + "y";
     }
 
     /**
      * Recursively generates a string of one or more Whatzits.
+     *
+     * @param n Number of Whazits to generate.
+     * @return n-amount of Whazits.
      */
     private String makeMultiWhatzits(int n) {
         return n == 0 ? "" : makeMultiWhatzits(n - 1) + makeWhatzit();
@@ -65,6 +82,8 @@ public class BlurbGenerator {
     /**
      * Generates a random Whatzit. A Whatzit is a 'q' followed by either a 'z' or a 'd', followed by
      * a Whoozit.
+     *
+     * @return A Whazit.
      */
     private String makeWhatzit() {
         return "q" + (getRandInt(0, 1) == 0 ? "d" : "z") + makeWhoozit();
