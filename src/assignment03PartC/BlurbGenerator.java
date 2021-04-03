b68eefedf14723fc2e23d2a1d945ab8a37ef82cb
mosguinz <mos.guinz@gmail.com>
2021-04-02 18:09:04 -0700

    Update blurb creation bounds

--- src/assignment03PartC/BlurbGenerator.java
+++ src/assignment03PartC/BlurbGenerator.java
@@ -13,17 +13,14 @@ package assignment03PartC;
 
 public class BlurbGenerator {
 
-    private static final int MIN_BLURB_SIZE = 4;
-    private static final int MAX_BLURB_SIZE = 21;
     private static final int MAX_WHOOZIT_Y_SIZE = 5;
-    private final int blurbSize;
+    private static final int MAX_WHAZITS = 5;
 
     /**
      * Instantiates a random number generator needed for blurb creation.
      */
     public BlurbGenerator() {
-        blurbSize = getRandInt(MIN_BLURB_SIZE, MAX_BLURB_SIZE);
-        System.out.println(blurbSize);
+        // Nothing here! We generate them on each blurbs.
     }
 
     /**
@@ -41,7 +38,7 @@ public class BlurbGenerator {
      * Generates and returns a random Blurb. A Blurb is a Whoozit followed by one or more Whatzits.
      */
     public String makeBlurb() {
-        return makeWhoozit() + makeMultiWhatzits(getRandInt(MIN_BLURB_SIZE, MAX_BLURB_SIZE));
+        return makeWhoozit() + makeMultiWhatzits(getRandInt(1, MAX_WHAZITS));
     }
 
     /**
